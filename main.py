f6a28792dc9c96fb3bc3492b97ab48f5b0378787
Dylan1939 <61760435+Dylan1939@users.noreply.github.com>
2020-12-01 19:38:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # This is the template code for the CNA337 Final Project
-# Zachary Rubin, zrubin@rtc.edu
+# Dylan McCormack
+#Worked with Eric Y, Micheal H and got help from Luma
 # CNA 337 Fall 2020
-
+from Server import Server
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Dylan")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    DylanLaunchWizard27 = Server('18.218.16.191')
     # TODO - Call Ping method and print the results
+    print(DylanLaunchWizard27.ping())
