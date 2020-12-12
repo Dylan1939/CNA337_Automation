d470da562cd1807e5a08c5b21f17c235af4abc68
Dylan1939 <61760435+Dylan1939@users.noreply.github.com>
2020-12-11 20:12:08 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -13,3 +13,23 @@ class Server:
         # TODO - Use os module to ping the server
         server = os.system("ping -n 4 " + self.server_ip)
         return self.server_ip
+
+# Dylan McCormack CNA 337
+# ssh automate
+# Worked with Eric Y, Got help from Matthew W, Luma N and Micheal H
+
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect('18.218.16.191', port=22, username='ec2-user', password='', key_filename='id_rsa')
+
+stdin, stdout, stderr = ssh.exec_command('''sudo yum-get update
+                                         sudo yum-get upgrade -y
+                                         ''')
+data = stdout.read().splitlines()
+for line in data:
+    print(line)
+
+ssh.close()
